84dd78170087ae0c47f10c4b2bb3a5dc9bc51f60
sam32190 <sameersaurav321@gmail.com>
2025-01-04 11:53:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,127 @@
+const redux = require("redux");
+const bindActionCreators = redux.bindActionCreators;
+const combineReducers = redux.combineReducers;
+const createStore = redux.createStore;
+
+// defining action.type
+const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED';
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED';
+
+// An action creator is a function which returns action as an object.
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    payload: 1,
+  };
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
+  };
+}
+
+function orderIceCream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function restockIceCream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
+// State of our application as an object
+const initialCakeState = {
+  numOfCakes: 10,
+};
+
+const initialIceCreamState = {
+  numOfIceCreams: 20,
+};
+
+// Reducers are functions that accepts state and action as arguments and returns a new state of the application
+const cakeReducer = (state = initialCakeState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes + action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams - 1,
+      };
+
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numOfIceCreams: state.numOfIceCreams + action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+// combining multiple reducers
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+
+// Step 1: Creating a store
+const store = createStore(rootReducer);
+
+// Step 2: Accessing state
+console.log("Initial state", store.getState());
+
+// Step 4: subscribe
+const unsubscribe = store.subscribe(() =>
+  console.log("Update state ", store.getState())
+);
+
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+
+// Step 3: allows state to be updated via dispatch
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIceCream, restockIceCream },
+  store.dispatch
+);
+
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(3);
+
+actions.orderIceCream();
+actions.orderIceCream();
+actions.restockIceCream(2);
+
+// Step 5: unsubscribe
+unsubscribe();
